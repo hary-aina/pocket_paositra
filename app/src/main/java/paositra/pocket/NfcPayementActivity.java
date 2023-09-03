6b948cef7918b609a62bf0706cbada4a3dee4823
hary-aina <harynyainarajaonah01@gmail.com>
2023-09-03 10:23:10 +0300

    design de carte avec logo NFC lors du paiement

--- app/src/main/java/paositra/pocket/NfcPayementActivity.java
+++ app/src/main/java/paositra/pocket/NfcPayementActivity.java
@@ -3,18 +3,25 @@ package paositra.pocket;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.nfc.NfcAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class NfcPayementActivity extends AppCompatActivity {
 
     //Declare NfcAdapter and PendingIntent
     private NfcAdapter nfcAdapter;
     private PendingIntent pendingIntent;
+    SharedPreferences preferences;
+    private final static String confPref = "conf_client";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +47,12 @@ public class NfcPayementActivity extends AppCompatActivity {
                 addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
         // single top flag avoids activity multiple instances launching
 
+        //chargement des informations du proprietaire
+        preferences = getSharedPreferences(confPref, Context.MODE_PRIVATE);
+        TextView tv_num_compte_card_value = (TextView) findViewById(R.id.num_compte_card_value);
+        TextView tv_proprietaire = (TextView) findViewById(R.id.proprietaire);
+        tv_num_compte_card_value.setText(preferences.getString("numero_compte", ""));
+        tv_proprietaire.setText(preferences.getString("nom", "")+" "+preferences.getString("prenom", ""));
 
         //finish
         ImageButton returnBtn = (ImageButton)findViewById(R.id.retour);
